81d27c8408710b6dcf8225c4d2290f1f26d31a0f
alessandroiero <alessandro.iero@outlook.it>
2023-03-23 09:13:10 +0100

    Primo Esercizio Usando Vite

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script >
+  // importiamo i componenti
+  import AppTitle from "./components/AppTitle.vue"
+
+
+  // componenti
+  export default {
+    components : {
+      AppTitle
+    }
+  }
+</script>
+
+<template>
+  <AppTitle></AppTitle>
+  <AppTitle></AppTitle>
+  <p>paragrafo app</p>
+</template>
+
+<style scoped>
+p{
+  color: green;
+}
+
+</style>
---
+++ src/components/AppTitle.vue
@@ -0,0 +1,23 @@
+<!-- contiene js -->
+<script>
+// dobbiamo esportarlo per poterlo importare
+  export default {
+    data(){
+      return{
+        message: "La mia prima app con Vite"
+      }
+    }
+  }
+</script>
+
+// <!-- contiene html -->
+<template>
+    <h1>{{ message }}</h1>
+    <p>Paragrafo componenti</p>
+</template>
+
+// <!-- contiene css -->
+<style scoped>
+p{
+  color: red;
+}</style>
